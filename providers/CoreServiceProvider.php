6cdab16cf7400cb3d6088891bd0b2bc18ab28b54
Duy Nguyen <duy.nguyen@go1.com.au>
2017-08-04 17:17:18 +0700

    Support cache.array for phpunit-6

--- providers/CoreServiceProvider.php
+++ providers/CoreServiceProvider.php
@@ -13,7 +13,7 @@ use Memcache;
 use Memcached;
 use Monolog\Handler\ErrorLogHandler;
 use Monolog\Logger;
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 use Psr\Log\LogLevel;
@@ -57,7 +57,7 @@ class CoreServiceProvider implements ServiceProviderInterface
                 case 'memcached':
                 case 'filesystem':
                 case 'redis':
-                    return class_exists(PHPUnit_Framework_TestCase::class, false) ? $c["cache.array"] : $c["cache.{$backend}"];
+                    return class_exists(TestCase::class, false) ? $c["cache.array"] : $c["cache.{$backend}"];
 
                 default:
                     if ($c->offsetExists("cache.{$backend}")) {
