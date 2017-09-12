8e59bc182eaa2776af3531d70c2e2bafc6fc221c
Andy Truong <thehongtt@gmail.com>
2017-09-12 10:21:19 +0700

    Profiler: Support rabbitMQ.

---
+++ domain/profiler/RabbitMqDataCollector.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace go1\app\domain\profiler;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerTrait;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+
+class RabbitMqDataCollector extends DataCollector implements LoggerInterface
+{
+    use LoggerTrait;
+
+    public function getName()
+    {
+        return 'mq';
+    }
+
+    public function log($level, $message, array $context = [])
+    {
+        if ('debug' === $level) {
+            $this->data[] = ['message' => $message, 'context' => $context];
+        }
+    }
+
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+        return $this->data;
+    }
+}
