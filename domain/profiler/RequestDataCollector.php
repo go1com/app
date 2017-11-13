3491d03f44063d9d0c2c46d26229e45b4e10a910
Phuong Huynh <phuonghuynh.net@gmail.com>
2017-11-13 17:00:01 +0700

    Remove sensitive data

--- domain/profiler/RequestDataCollector.php
+++ domain/profiler/RequestDataCollector.php
@@ -12,14 +12,42 @@ class RequestDataCollector extends OriginalRequestDataCollector
     {
         parent::collect($request, $response, $exception);
 
-        $sensorKeys = ['password', 'private-key', 'private_key'];
+        $sensorKeys = [
+            'password',
+            'private-key',
+            'private_key',
+            'secret',
+            'app_debug',
+            'cache_backend',
+            'cache_host',
+            'cache_port',
+            'content_length',
+            'content_type',
+            'dev_db_host',
+            'dev_db_password',
+            'dev_db_username',
+            'document_root',
+            'document_uri',
+            'env',
+            'fcgi_role',
+            'gateway_interface',
+            'go1_db_host',
+            'go1_db_name',
+            'go1_db_password',
+            'go1_db_port',
+            'go1_db_slave',
+            'go1_db_username',
+            'home',
+            'hostname',
+            'http_authorization',
+        ];
         $dataKeys = ['request_server', 'request_headers'];
 
         foreach ($dataKeys as $dataKey) {
             foreach ($this->data[$dataKey] as $key => $value) {
                 foreach ($sensorKeys as $sensorKey) {
-                    if (strstr(strtolower($key), $sensorKey)) {
-                        $this->data[$dataKey][$key] = '***';
+                    if (false !== strpos(strtolower($key), $sensorKey)) {
+                        unset($this->data[$dataKey][$key]);
                         break;
                     }
                 }
--- tests/RequestDataCollectorTest.php
+++ tests/RequestDataCollectorTest.php
@@ -11,18 +11,56 @@ class RequestDataCollectorTest extends TestCase
 {
     public function testSensorSensitiveData()
     {
+        $sensorKeys = [
+            'password',
+            'private-key',
+            'private_key',
+            'secret',
+            'app_debug',
+            'cache_backend',
+            'cache_host',
+            'cache_port',
+            'content_length',
+            'content_type',
+            'dev_db_host',
+            'dev_db_password',
+            'dev_db_username',
+            'document_root',
+            'document_uri',
+            'env',
+            'fcgi_role',
+            'gateway_interface',
+            'go1_db_host',
+            'go1_db_name',
+            'go1_db_password',
+            'go1_db_port',
+            'go1_db_slave',
+            'go1_db_username',
+            'home',
+            'hostname',
+            'http_authorization',
+        ];
+
         $c = new RequestDataCollector();
         $req = Request::create('/');
         $req->headers->set('jwt-private-key', 'test');
         $req->server->set('GO1_DB_PASSWORD', 'db password');
         $req->server->set('HTTP_JWT_PRIVATE_KEY', 'private key');
 
+        foreach ($sensorKeys as $sensorKey) {
+            $req->server->set(strtoupper($sensorKey), 'test');
+        }
+
         $res = Response::create();
         $c->collect($req, $res);
         $c->lateCollect();
 
-        $this->assertEquals('***', $c->getRequestHeaders()->get('jwt-private-key'));
-        $this->assertEquals('***', $c->getRequestServer()->get('GO1_DB_PASSWORD'));
-        $this->assertEquals('***', $c->getRequestServer()->get('HTTP_JWT_PRIVATE_KEY'));
+        $this->assertFalse($c->getRequestHeaders()->has('jwt-private-key'));
+        $this->assertFalse($c->getRequestServer()->has('GO1_DB_PASSWORD'));
+        $this->assertFalse($c->getRequestServer()->has('HTTP_JWT_PRIVATE_KEY'));
+
+        foreach ($sensorKeys as $sensorKey) {
+            $this->assertFalse($c->getRequestServer()->has($sensorKey));
+        }
     }
 }
