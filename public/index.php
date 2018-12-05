91374377e62237d7430781077164971597002e16
Andy Truong <thehongtt@gmail.com>
2018-12-06 09:58:33 +1000

    Fxi public/index.php

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ return call_user_func(function () {
 
     require_once APP_ROOT . '/vendor/autoload.php';
     $cnf = is_file(APP_ROOT . '/resources/config.php') ? APP_ROOT . '/config.php' : APP_ROOT . '/resources/config.default.php';
+    $cnf = require $cnf;
     $app = new DomainService($cnf);
 
     return ('cli' === php_sapi_name()) ? $app : $app->run();
