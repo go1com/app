524054d3c20ac9e9598472a7228f3e5e2ec8818d
Andy Truong <thehongtt@gmail.com>
2018-12-18 14:02:23 +1000

    DomainServiceTestCase > provide more stub.

--- tests/DomainServiceTestCase.php
+++ tests/DomainServiceTestCase.php
@@ -2,18 +2,42 @@
 
 namespace go1\app\tests;
 
+use Doctrine\DBAL\DriverManager;
 use go1\app\DomainService;
+use go1\util\schema\InstallTrait;
 use PHPUnit\Framework\TestCase;
 use RuntimeException;
 
 abstract class DomainServiceTestCase extends TestCase
 {
+    use InstallTrait;
+
+    protected $sqlite;
+
     protected function getApp(): DomainService
     {
         if (!defined('APP_ROOT')) {
             throw new RuntimeException('APP_ROOT is not defined');
         }
 
-        return require __DIR__ . '/../public/index.php';
+        /** @var DomainService $app */
+        $app = require __DIR__ . '/../public/index.php';
+        $app['dbs'] = $app->extend('dbs', function () { return $this->getDatabases(); });
+        $this->appInstall($app);
+
+        return $app;
+    }
+
+    protected function appInstall(DomainService $app)
+    {
+        $this->installGo1Schema($app['dbs']['go1'], $coreOnly = false);
+    }
+
+    protected function getDatabases()
+    {
+        return [
+            'default' => $this->sqlite = DriverManager::getConnection(['url' => 'sqlite://sqlite::memory:']),
+            'go1'     => $this->sqlite,
+        ];
     }
 }
