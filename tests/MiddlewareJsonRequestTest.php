7240be8d1786e706ae40171e46ae468419fb2ad8
Marc Rubio <marc.rubio@go1.com>
2019-11-11 09:35:23 +1000

    GO1P-28029 Do not parse JSON body for GET/HEAD requests and return parsing errors

--- tests/MiddlewareJsonRequestTest.php
+++ tests/MiddlewareJsonRequestTest.php
@@ -6,6 +6,7 @@ use go1\app\App;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class MiddlewareJsonRequestTest extends TestCase
 {
@@ -20,5 +21,19 @@ class MiddlewareJsonRequestTest extends TestCase
         $req->headers->set('Content-Type', 'application/json');
         $res = $app->handle($req);
         $this->assertEquals($body, $res->getContent());
+    }
+
+    public function testItReturnsJsonParsingError()
+    {
+        $app = new App();
+        $app->post('/call-me', function (Request $req) use (&$called) {
+            return new JsonResponse($req->request->all());
+        });
+
+        $req = Request::create('/call-me', 'POST', [], [], [], [], '{"foo":"bar"');
+        $req->headers->set('Content-Type', 'application/json');
+        $res = $app->handle($req);
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $res->getStatusCode());
+        $this->assertStringContainsString('Invalid JSON payload', $res->getContent());
     }
 }
